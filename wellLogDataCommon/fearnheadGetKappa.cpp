2c32ac2bc265995ab1528f7b18b2f937014b0b0d
Rohan Shah <github@bookshah.com>
2016-12-19 00:42:50 +1000

    fearnheadGetKappa now works.

--- wellLogDataCommon/fearnheadGetKappa.cpp
+++ wellLogDataCommon/fearnheadGetKappa.cpp
@@ -2,49 +2,36 @@
 #include <boost/random/uniform_int_distribution.hpp>
 namespace wellLogData
 {
-	double fearnheadGetKappa(std::size_t size, std::function<double(std::size_t)> getWeight, boost::mt19937& randomSource, int N, int& A, double& B)
+	void fearnheadGetKappa(std::size_t size, std::function<double(std::size_t)> getWeight, boost::mt19937& randomSource, int N, int& A, double& B)
 	{
 		int lowerBound = 0, upperBound = (int)size-1;
 		double lowerBoundSum = 0;
 		const double tolerance = 1e-8;
-		while(getWeight(lowerBound) != getWeight(upperBound))
+		while(true)
 		{
 			//Split the existing partition between currentIndex and currentIndex2
-			int currentIndex1 = boost::random::uniform_int_distribution<>(lowerBound, upperBound-1)(randomSource);
-			int currentIndex2 = currentIndex1 + 1;
-			double partitionWeight1 = getWeight(currentIndex1), partitionWeight2 = getWeight(currentIndex2);
-			if(partitionWeight1 == partitionWeight2)
+			int currentIndex = boost::random::uniform_int_distribution<>(lowerBound, upperBound-1)(randomSource);
+			while(currentIndex <= upperBound - 1 && getWeight(currentIndex) == getWeight(currentIndex+1)) currentIndex++;
+			if(currentIndex == (int)size - 1)
 			{
+				A = 0;
 				B = lowerBoundSum;
-				A = (int)(size - lowerBound);
-				for(int i = lowerBound; getWeight(i) <= partitionWeight1 && i < (int)size; i++)
-				{
-					B += getWeight(i);
-					A--;
-				}
-				if(B / partitionWeight1 + A <= N + tolerance)
-				{
-					upperBound = currentIndex1;
-				}
-				else
-				{
-					lowerBoundSum = B;
-					lowerBound = (int)size - A;
-					if(A == 0) goto skipCheck;
-				}
+				for(int i = lowerBound; i <= upperBound; i++) B += getWeight(i);
 			}
 			else
 			{
+				double partitionWeight1 = getWeight(currentIndex), partitionWeight2 = getWeight(currentIndex+1);
 				double B1 = lowerBoundSum;
 				int A1 = (int)size - lowerBound;
-				for(int i = lowerBound; getWeight(i) <= partitionWeight1 && i < (int)size; i++)
+				int i = lowerBound;
+				for(; getWeight(i) < partitionWeight1 && i < (int)size; i++)
 				{
 					B1 += getWeight(i);
 					A1--;
 				}
 				double B2 = B1;
 				int A2 = A1;
-				for(int i = currentIndex2; getWeight(i) <= partitionWeight2 && i < (int)size; i++)
+				for(; getWeight(i) < partitionWeight2 && i < (int)size; i++)
 				{
 					B2 += getWeight(i);
 					A2--;
@@ -52,39 +39,47 @@ namespace wellLogData
 				//We've found the minimum
 				if(B1 / partitionWeight1 + A1 > N + tolerance && B2 / partitionWeight2 + A2 <= N + tolerance)
 				{
-					upperBound = lowerBound = currentIndex2;
 					B = B2;
 					A = A2;
-					goto skipCheck;
+					return;
 				}
 				//Take the smaller half
 				else if(B1 / partitionWeight1 + A1 <= N + tolerance)
 				{
-					upperBound = currentIndex1;
-					A = A1;
-					B = B1;
+					upperBound = currentIndex;
 				}
 				//Take the bigger half
 				else
 				{
-					lowerBound = currentIndex2;
-					lowerBoundSum = B1;
-					B = B2;
-					A = A2;
+					lowerBound = currentIndex + 1;
+					lowerBoundSum = B2;
+				}
+			}
+			if(getWeight(lowerBound) == getWeight(upperBound))
+			{
+				double partitionWeight = getWeight(lowerBound);
+				B = lowerBoundSum;
+				A = (int)size - lowerBound;
+				int i = lowerBound;
+				for(; getWeight(i) < partitionWeight; i++)
+				{
+					B += getWeight(i);
+					A--;
+				}
+				if(B / partitionWeight + A <= N + tolerance)
+				{
+					return;
+				}
+				else
+				{
+					for(; i <= upperBound; i++)
+					{
+						B += getWeight(i);
+						A--;
+					}
+					return;
 				}
 			}
 		}
-		{
-			double partitionWeight = getWeight(lowerBound);
-			A = (int)size - lowerBound;
-			B = lowerBoundSum;
-			for(int i = lowerBound; getWeight(i) <= partitionWeight && i < (int)size; i++)
-			{
-				B += getWeight(i);
-				A--;
-			}
-		}
-skipCheck:
-		return getWeight(lowerBound);
 	}
 }
--- wellLogDataCommon/fearnheadGetKappa.h
+++ wellLogDataCommon/fearnheadGetKappa.h
@@ -4,6 +4,6 @@
 #include <boost/random/mersenne_twister.hpp>
 namespace wellLogData
 {
-	double fearnheadGetKappa(std::size_t size, std::function<double(std::size_t)> getWeight, boost::mt19937& randomSource, int N, int& A, double& B);
+	void fearnheadGetKappa(std::size_t size, std::function<double(std::size_t)> getWeight, boost::mt19937& randomSource, int N, int& A, double& B);
 }
 #endif
